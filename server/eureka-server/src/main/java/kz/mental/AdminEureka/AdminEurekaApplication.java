443bd7c213ab7da1357c94a38253367bb3234358
Meruert <meruertsajlauova@gmail.com>
2025-03-24 16:21:56 +0500

    dev(): back initial commit, add new eureka server

---
+++ server/eureka-server/src/main/java/kz/mental/AdminEureka/AdminEurekaApplication.java
@@ -0,0 +1,13 @@
+package kz.mental.AdminEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AdminEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminEurekaApplication.class, args);
+    }
+}
---
+++ server/eureka-server/src/test/java/kz/mental/AdminEureka/AdminEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.mental.AdminEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
